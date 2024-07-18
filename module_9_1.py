c55a09f59a6d3bb551a3d876c2eb6463dfa6abdd
ncidorowa <166540449+ncidorowa@users.noreply.github.com>
2024-07-18 20:52:20 +0400

    Add files via upload

---
+++ module_9_1.py
@@ -0,0 +1,9 @@
+def apply_all_func(int_list, *functions):
+    reuslts = {}
+    for i in functions:
+        reuslts[i.__name__] = i(int_list)
+    return reuslts
+
+
+print(apply_all_func([6, 20, 15, 9],  max, min), (apply_all_func([6, 20, 15, 9],  len, sum, sorted)))
+#print(apply_all_func([6, 20, 15, 9],  len, sum, sorted))
